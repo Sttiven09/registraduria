dd773c458e2fd8bc7eb6d545cfb93c2a6981df14
Edwin Ramirez <edwin.sti.ramirez09@gmail.com>
2022-11-14 00:50:19 -0500

    configurando para render v2

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -o errexit
 
 #poetry install
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-pip install requirements.txt
+
 
 #python manage.py migrate
--- consultaciudadanoProject/settings.py
+++ consultaciudadanoProject/settings.py
@@ -16,6 +16,7 @@ import os
 import dj_database_url
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -203,13 +204,13 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 REST_FRAMEWORK = {
