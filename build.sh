a257f8376bfa41ae830668cc5f32591a5173c3a2
Edwin Ramirez <edwin.sti.ramirez09@gmail.com>
2022-11-14 13:48:35 -0500

    arreglando vistas ciudadanos v.1

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 set -o errexit
 
 #poetry install
-pip install -r requirements.txt
+
+#comentado activar cuando se instalen mas dependencias
+#pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
