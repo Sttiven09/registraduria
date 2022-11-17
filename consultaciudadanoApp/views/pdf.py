3a572d0aa2954bccccaa0d3d2263d8869cd68fac
Edwin Ramirez <edwin.sti.ramirez09@gmail.com>
2022-11-17 12:29:06 -0500

    prueba pdf

---
+++ consultaciudadanoApp/views/pdf.py
@@ -0,0 +1,27 @@
+from django.shortcuts import HttpResponse
+from django.views import View
+from consultaciudadanoApp.models import Ciudadanos
+
+import os
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+
+class generarPDF(View):
+    def get(self, *args, **kwargs):
+        
+        template = get_template('ciudadanos.html')
+        context = {
+            'ciudadanos': Ciudadanos.objects.all(),
+            
+            }
+        html = template.render(context)
+        response = HttpResponse(content_type='application/pdf')
+        #response['Content-Disposition'] = 'attachment; filename="reporte.pdf"'
+        pisa_status = pisa.CreatePDF(html, dest=response)
+        if pisa_status.err:
+            return HttpResponse('err' + html + '</pre>')
+        return response
+
